b40b8071c83e841c46c737c76135f590e637d238
Luis Ruediger <luisruediger27@gmail.com>
2023-07-17 14:33:55 -0300

    criando carrossel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import { HomeContainer, Product } from '@/styles/pages/home'
 import Image from 'next/image'
+import { useKeenSlider } from 'keen-slider/react'
 
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [slidesRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={slidesRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} height={480} width={520} alt="" />
 
         <footer>
@@ -15,7 +24,23 @@ export default function Home() {
           <span>R$ 79.90 </span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} height={480} width={520} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79.90 </span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} height={480} width={520} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79.90 </span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} height={480} width={520} alt="" />
 
         <footer>
