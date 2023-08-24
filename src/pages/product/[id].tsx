a95d653f8400333e4b0d27a177135a2706806d5c
Luis Ruediger <luisruediger27@gmail.com>
2023-08-24 16:20:19 -0300

    ssg com parametro dinamico

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,7 +4,7 @@ import {
   ProductContainer,
   ProductDetails,
 } from '@/styles/pages/product'
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
 import Stripe from 'stripe'
 
@@ -35,6 +35,13 @@ export default function Product({ product }: ProductProps) {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: 'prod_OHnT4lLxDnDIWJ' } }],
+    fallback: false,
+  }
 }
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
